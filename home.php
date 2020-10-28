10b01d3e97e2a0e135254a7039f5c5e4502f26bc
Joseph Then <josephthen3320@outlook.com>
2020-10-28 11:58:03 +0800

    Updated all official roles.

--- home.php
+++ home.php
@@ -139,7 +139,7 @@
         <div class="w3-col l3 m6 w3-margin-bottom">
             <img src="img/members/joseph.jpg" alt="Joseph" style="width:100%">
             <h3>Joseph Then</h3>
-            <p class="w3-opacity">Digital Overlord (Team Leader)<br><span class="w3-small">Team Leader</span></p>
+            <p class="w3-opacity">Digital Overlord<br><span class="w3-small">Team Leader</span></p>
             <p>Joseph is a free-spirited, yet passionate leader who will always push and support the team to a higher ground.</p>
             <h5>
                 <a href="mailto:scyjt1@nottingham.edu.cn" class="w3-button w3-light-grey w3-block w3-half w3-hide-small">Contact <i class="fa fa-fw fa-paper-plane"></i></a>
@@ -153,7 +153,7 @@
         <div class="w3-col l3 m6 w3-margin-bottom">
             <img src="img/members/patricia.jpg" alt="Patricia" style="width:100%">
             <h3>Patricia Wong</h3>
-            <p class="w3-opacity">Strategist (Documentation Manager)</p>
+            <p class="w3-opacity">Strategist<br><span class="w3-small">Documentation Manager</span></p>
             <p>A fast learner, Patricia adapts quickly to changing situations and always pursues knowledge with curiosity.<br></p>
             <h5>
                 <a href="mailto:smyww1@nottingham.edu.cn" class="w3-button w3-light-grey w3-block w3-half w3-hide-small">Contact <i class="fa fa-fw fa-paper-plane"></i></a>
@@ -167,7 +167,7 @@
         <div class="w3-col l3 m6 w3-margin-bottom">
             <img src="img/members/ivan.jpg" alt="Ivan" style="width:100%">
             <h3>Ivan Halim</h3>
-            <p class="w3-opacity">The Handyman</p>
+            <p class="w3-opacity">The Handyman<br><span class="w3-small">Technical Lead (Back-End Dev)</span></p>
             <p>Sanguine and optimistic. Ivan is the man who will always lighten up our team with his happy personality.<br></p>
             <h5>
                 <a href="mailto:scyih1@nottingham.edu.cn" class="w3-button w3-light-grey w3-block w3-half w3-hide-small">Contact <i class="fa fa-fw fa-paper-plane"></i></a>
@@ -181,7 +181,7 @@
         <div class="w3-col l3 m6 w3-margin-bottom">
             <img src="img/members/gabrielle.jpg" alt="Gabrielle" style="width:100%">
             <h3>Gabrielle Hadian</h3>
-            <p class="w3-opacity">Chief Multitasker</p>
+            <p class="w3-opacity">Chief Multitasker<br><span class="w3-small">UI/UX Lead Developer (Front-End Dev)</span></p>
             <p>Hardworking and dependable. Gabrielle never fails to motivate and bring the best out of the team in the most challenging times.</p>
             <h5>
                 <a href="mailto:scygh2@nottingham.edu.cn" class="w3-button w3-light-grey w3-block w3-half w3-hide-small">Contact <i class="fa fa-fw fa-paper-plane"></i></a>
@@ -196,7 +196,7 @@
         <div class="w3-col l3 m6 w3-margin-bottom">
             <img src="img/members/aurelie.jpg" alt="Aurelie" style="width:100%">
             <h3>Aurelie U-King Im</h3>
-            <p class="w3-opacity">Analyst</p>
+            <p class="w3-opacity">Analyst<br><span class="w3-small">Project Manager</span></p>
             <p>Forte in small details, Aurelie is thoroughly active and takes initiative to take care of details and helps troubled teammates.</p>
             <h5>
                 <a href="mailto:scyau1@nottingham.edu.cn" class="w3-button w3-light-grey w3-block w3-half w3-hide-small">Contact <i class="fa fa-fw fa-paper-plane"></i></a>
@@ -210,7 +210,7 @@
         <div class="w3-col l3 m6 w3-margin-bottom">
             <img src="img/members/kevin.jpg" alt="Kevin" style="width:100%">
             <h3>Kevin Ferdinand</h3>
-            <p class="w3-opacity">Jack of All Trades</p>
+            <p class="w3-opacity">Jack of All Trades<br><span class="w3-small">Code Developer</span></p>
             <p>Thinking outside the box, Kevin is quick-witted and often sees things from different perspectives to come up with novel solutions.</p>
             <h5>
                 <a href="mailto:scykf2@nottingham.edu.cn" class="w3-button w3-light-grey w3-block w3-half w3-hide-small">Contact <i class="fa fa-fw fa-paper-plane"></i></a>
@@ -224,7 +224,7 @@
         <div class="w3-col l3 m6 w3-margin-bottom">
             <img src="img/members/kelvin.jpg" alt="Kelvin" style="width:100%">
             <h3>Li-Kai (Kelvin) Wu</h3>
-            <p class="w3-opacity">Bug Whisperer</p>
+            <p class="w3-opacity">Bug Whisperer<br><span class="w3-small">Repository Assisstant Manager</span></p>
             <p>Friendly and diligent. Kelvin strives to always explore new experiences and will be happy to give and receive suggestions to the team.</p>
             <h5>
                 <a href="mailto:scylw1@nottingham.edu.cn" class="w3-button w3-light-grey w3-block w3-half w3-hide-small">Contact <i class="fa fa-fw fa-paper-plane"></i></a>
@@ -239,7 +239,7 @@
             <img src="img/members/supervisor.jpg" alt="Supervisor" style="width:100%">
             <!--img src="https://www.nottingham.edu.cn/image-library/Staff-Profile/Engineering/dave-towey.x19ec4d43.png" alt="Dave Towey (Supervisor)" style="width:100%"-->
             <h3>Dave Towey</h3>
-            <p class="w3-opacity">Project Supervisor</p>
+            <p class="w3-opacity">Project Supervisor<br><span class="w3-small">and Module Convener</span></p>
             <p>(Self-claimed) Harshest supervisor for the best project there is in the GRP Module. Very nice and supportive guy. <br></p>
             <p><a href="mailto:dave.towey@nottingham.edu.cn" class="w3-button w3-light-grey w3-block">Contact</a></p>
         </div>
